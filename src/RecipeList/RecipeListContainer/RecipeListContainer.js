cccd39c8ad5696028d40328f450cc3d25d51954a
Drew Love <davidandrewlove84@gmail.com>
2021-04-25 12:08:13 -0400

    Use setRecipeList in RecipeListContainer

--- src/RecipeList/RecipeListContainer/RecipeListContainer.js
+++ src/RecipeList/RecipeListContainer/RecipeListContainer.js
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import RecipeList from "../RecipeList/RecipeList";
 
 function RecipeListContainer() {
-  const [recipeList, setRecipeList] = useState([
-    { recipe_id: 1, recipe_name: "Sourdough" },
-    { recipe_id: 2, recipe_name: "Challah" },
-    { recipe_id: 3, recipe_name: "Focaccia" },
-  ]);
+  const [recipeList, setRecipeList] = useState([]);
+
+  useEffect(() => {
+    const recipes = [
+      { recipe_id: 1, recipe_name: "Sourdough" },
+      { recipe_id: 2, recipe_name: "Challah" },
+      { recipe_id: 3, recipe_name: "Focaccia" },
+    ];
+
+    setRecipeList(recipes);
+  }, []);
 
   return <RecipeList recipeList={recipeList} />;
 }
