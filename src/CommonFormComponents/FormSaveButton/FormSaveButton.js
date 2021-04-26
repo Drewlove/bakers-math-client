943f7def2fdedbf5963144b6e31c1355fd6115cd
Drew Love <davidandrewlove84@gmail.com>
2021-04-26 18:14:17 -0400

    Comment out unused functions in FormSaveButton, will reimplement later once ready for modal logic and redirecting

--- src/CommonFormComponents/FormSaveButton/FormSaveButton.js
+++ src/CommonFormComponents/FormSaveButton/FormSaveButton.js
@@ -1,40 +1,47 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+import React from "react";
 // import Modal from "../../../Modal/Modal";
 import { API_SAVE } from "../../Utilities/APImethods/API_SAVE";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 // import { GET_ALL_ERROR_MESSAGES } from "../../ValidateForm/GET_ALL_ERROR_MESSAGES";
 
 function FormSaveButton(props) {
-  const history = useHistory();
-  const [resSave, saveData] = API_SAVE(
+  // const history = useHistory();
+  // const [resSave, saveData] = API_SAVE(
+  //   props.formData,
+  //   props.endpointSuffix,
+  //   props.rowId
+  // );
+  const [saveData] = API_SAVE(
     props.formData,
     props.endpointSuffix,
     props.rowId
   );
-  const [modal, setModal] = useState({
-    display: false,
-  });
 
-  useEffect(() => {
-    if (resSave.saveSuccessful)
-      return setModal({
-        display: true,
-        type: "notification",
-        text: "Save successful.",
-        redirect: true,
-        redirectSuffix: props.redirectSuffix,
-      });
+  // const [modal, setModal] = useState({
+  //   display: false,
+  // });
 
-    if (resSave.saveError.length > 0) {
-      return setModal({
-        display: true,
-        type: "notification",
-        text: "Failed to save.",
-        redirect: false,
-        redirectSuffix: "",
-      });
-    }
-  }, [resSave, props.redirectSuffix]);
+  // useEffect(() => {
+  //   if (resSave.saveSuccessful)
+  //     return setModal({
+  //       display: true,
+  //       type: "notification",
+  //       text: "Save successful.",
+  //       redirect: true,
+  //       redirectSuffix: props.redirectSuffix,
+  //     });
+
+  //   if (resSave.saveError.length > 0) {
+  //     return setModal({
+  //       display: true,
+  //       type: "notification",
+  //       text: "Failed to save.",
+  //       redirect: false,
+  //       redirectSuffix: "",
+  //     });
+  //   }
+  // }, [resSave, props.redirectSuffix]);
 
   // const hideModal = (e) => {
   //   e.preventDefault();
