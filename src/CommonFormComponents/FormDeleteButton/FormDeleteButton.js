e542c4f57357609ac3a4ef8148c2ff68aaf7bca6
Drew Love <davidandrewlove84@gmail.com>
2021-04-27 10:35:46 -0400

    Fix console errors that prevented deployment, remove useState and useEffect imports

--- src/CommonFormComponents/FormDeleteButton/FormDeleteButton.js
+++ src/CommonFormComponents/FormDeleteButton/FormDeleteButton.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+import React from "react";
 import { API_DELETE } from "../../Utilities/APImethods/API_DELETE";
 // import Modal from "../../../Modal/Modal";
 // import { useHistory } from "react-router-dom";
